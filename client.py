ebf0f0d01d6047590c8be37a63f901d287ac92c1
Purnima Grover <purnima.grover@students.iiit.ac.in>
2023-04-17 23:06:12 +0530

    Delete file code added

--- client.py
+++ client.py
@@ -34,11 +34,14 @@
 import socket
 import pdb,os
 import paramiko
-
+import Pyro4
 from paramiko import SSHClient
 from scp import SCPClient
 
 dir_list = []
+deleted_files = []
+ip_add = '10.2.133.164'
+port ='22'
 
 def createSSHClient(server, port, user, password):
     client = paramiko.SSHClient()
@@ -47,39 +50,57 @@ def createSSHClient(server, port, user, password):
     client.connect(server, port, user, password, look_for_keys=False, allow_agent=False)
     return client
 
-def dir_scanner():
-	temp = os.listdir('./temp')
-	new_files = [file for file in temp if file not in dir_list]
-	for i in new_files:
-		dir_list.append(i)
-	if len(new_files)!=0:
-		print(new_files)
-		for file in new_files:
-			ssh = createSSHClient('10.2.133.164','22', 'nilesh', '041997')
-			scp = SCPClient(ssh.get_transport())
-			scp.put('./temp/'+file, remote_path = '/home/nilesh/Documents/Distributed_Systems/FileSyncron/temp', recursive=True)
-			scp.close()
+def delete_file(deleted_files, s):
+    if(len(deleted_files) > 0):
+        del_msg = 'delete '
+        for file in deleted_files:
+            del_msg += str(file) + ' '
+    s.sendall(bytes(del_msg,'UTF-8'))
+
+def dir_scanner(s):
+    temp = os.listdir('./temp')
+    new_files = [file for file in temp if file not in dir_list]
+    deleted_files = [file for file in dir_list if file not in temp]
+    for i in new_files:
+        dir_list.append(i)
+    if len(new_files)!=0:
+        print(new_files)
+        for file in new_files:
+            ssh = createSSHClient(ip_add, port, 'nilesh', '041997')
+            scp = SCPClient(ssh.get_transport())
+            scp.put('./temp/'+file, remote_path = '/home/nilesh/Documents/Distributed_Systems/FileSyncron/temp', recursive=True)
+            scp.close()
+    delete_file(deleted_files, s)
+
+def detect_deleted_file_from_master():
+    master = Pyro4.core.Proxy('PYRO:Master@' + ip_add + ':9090')
+    msg = (master.check_deleted_file())
+    print(msg)
+    # file_list = msg.split(' ')[1:]
+    # for file in file_list:
+    #     file_path = './temp/' + file
+    #     try:
+    #         os.remove(file_path)
+    #     except FileNotFoundError:
+    #         print(f"File {file_path} does not exist.")
+    
+    
 
 def Main1():
-    # local host IP '127.0.0.1'
-    # pdb.set_trace()
-    
-    host = '10.2.133.164'
     port = 8083
-
     s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    
     # connect to server on local computer
-    s.connect((host,port))
+    s.connect((ip_add,port))
     s.sendall(bytes("purnima Ketan1411 " + os.getcwd() +"/temp",'UTF-8'))
 
-    ssh = createSSHClient( '10.2.133.164','22', 'nilesh', '041997')
+    ssh = createSSHClient( ip_add, port, 'nilesh', '041997')
     scp = SCPClient(ssh.get_transport())
     scp.get(local_path='./', remote_path = '/home/nilesh/Documents/Distributed_Systems/FileSyncron/temp', recursive=True)
     scp.put('./temp', remote_path = '/home/nilesh/Documents/Distributed_Systems/FileSyncron', recursive=True)
     scp.close()  
     while True:
-        dir_scanner()   
+        dir_scanner(s)
+        detect_deleted_file_from_master()
     s.close()
 
 Main1()
--- master.py
+++ master.py
@@ -1,10 +1,27 @@
 # import socket programming library
 import socket
 import os
-
+import Pyro4
 import paramiko
 from paramiko import SSHClient
 from scp import SCPClient
+# import thread module
+from _thread import *
+import threading
+# print_lock = threading.Lock()
+ip_list = dict()
+dir_list = []
+
+@Pyro4.expose
+class Master(object):
+    def check_deleted_file(self):
+        port = 8084
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind(('0.0.0.0', port))
+        c, addr = s.accept()
+        del_file = c.recv(8192).decode('utf-8')
+        return del_file
+    
 def createSSHClient(server, port, user, password):
     client = paramiko.SSHClient()
     client.load_system_host_keys()
@@ -12,99 +29,79 @@ def createSSHClient(server, port, user, password):
     client.connect(server, port, user, password, look_for_keys=False, allow_agent=False)
     return client
 
-
-# import thread module
-from _thread import *
-import threading
-
-# print_lock = threading.Lock()
-ip_list = dict()
-dir_list = []
-
 def dir_scanner():
-	while True:
-		temp = os.listdir('./temp')
-		new_files = [file for file in temp if file not in dir_list]
-		for i in new_files:
-			dir_list.append(i)
-		if len(new_files)!=0:
-			print(new_files)
-			for file in new_files:
-				# print(file)
-				for current_ip,data in ip_list.items():
-					# print(current_ip,data)
-					ssh = createSSHClient( current_ip,'22', data[0], data[1])
-					scp = SCPClient(ssh.get_transport())
-					scp.put('./temp/'+file, remote_path = data[2], recursive=True)
-					scp.close()
-
-
-
-
+    while True:
+        temp = os.listdir('./temp')
+        new_files = [file for file in temp if file not in dir_list]
+        for i in new_files:
+            dir_list.append(i)
+        if len(new_files)!=0:
+            print(new_files)
+            for file in new_files:
+                # print(file)
+                for current_ip,data in ip_list.items():
+                    # print(current_ip,data)
+                    ssh = createSSHClient( current_ip,'22', data[0], data[1])
+                    scp = SCPClient(ssh.get_transport())
+                    scp.put('./temp/'+file, remote_path = data[2], recursive=True)
+                    scp.close()
 
 # thread function
 def threaded(c,ip):
+    data = c.recv(1024).decode('utf-8')
+    while True:
+        # data received from client
+        if not data:
+            print('Bye')
+            # lock released on exit            # print_lock.release()
+            break
 
-	data = c.recv(1024).decode('utf-8')
+        # reverse the given string from client
+        data = data[::-1]
 
-	
+        # send back reversed string to client
+        c.send(data.encode('utf-8'))
 
-	while True:
-
-		# data received from client
-		if not data:
-			print('Bye')
-			
-			# lock released on exit
-			# print_lock.release()
-			break
-
-		# reverse the given string from client
-		data = data[::-1]
-
-		# send back reversed string to client
-		c.send(data.encode('utf-8'))
-
-	# connection closed
-	ip_list.pop(ip)
-	c.close()
+    # connection closed
+    ip_list.pop(ip)
+    c.close()
 
 
 def Main():
-	host = ""
+    host = ""
 
-	# reserve a port on your computer
-	# in our case it is 12345 but it
-	# can be anything
-	port = 8083
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.bind(('0.0.0.0', port))
-	print("socket binded to port", port)
+    # reserve a port on your computer
+    # in our case it is 12345 but it
+    # can be anything
+    port = 8083
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('0.0.0.0', port))
+    print("socket binded to port", port)
 
-	# put the socket into listening mode
-	s.listen(5)
-	print("socket is listening")
-	start_new_thread(dir_scanner, ())
-	# a forever loop until client wants to exit
-	while True:
+    # put the socket into listening mode
+    s.listen(5)
+    print("socket is listening")
+    start_new_thread(dir_scanner, ())
+    # a forever loop until client wants to exit
+    while True:
 
-		# establish connection with client
-		c, addr = s.accept()
-		data = c.recv(2048).decode('utf-8')
-		data1 = data.split(' ')
-		# lock acquired by client
-		# print_lock.acquire()
-		print('Connected to :', addr[0], ':', addr[1])
-		addr
-		ip_list[addr[0]] = data1
-		# Start a new thread and return its identifier
-		start_new_thread(threaded, (c,addr[0]))
-		print(ip_list)
-	s.close()
+        # establish connection with client
+        c, addr = s.accept()
+        data = c.recv(2048).decode('utf-8')
+        data1 = data.split(' ')
+        # lock acquired by client
+        # print_lock.acquire()
+        print('Connected to :', addr[0], ':', addr[1])
+        addr
+        ip_list[addr[0]] = data1
+        # Start a new thread and return its identifier
+        start_new_thread(threaded, (c,addr[0]))
+        print(ip_list)
+    s.close()
 
 
 if __name__ == '__main__':
-	Main()
+    Main()
 
 
 
